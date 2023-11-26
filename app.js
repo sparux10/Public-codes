9c751c6653e72c18da6583ac5cc3cf2225f42b1f
unknown <maribiamine@gmail.com>
2023-11-26 15:36:22 +0100

    add files

---
+++ app.js
@@ -0,0 +1,36 @@
+
+function al_ert(){
+
+    const button = document.createElement('button')
+    button.style.width = "80px"
+    button.style.border = "none"
+    button.style.padding = "1%"
+    button.style.backgroundColor = "green"
+    button.style.color = "white"
+    button.style.borderRadius = "5px"
+    button.innerText = "ok"
+
+    const dbtn = document.createElement('div')
+    dbtn.style.width = "100%"
+    dbtn.style.display = "flex"
+    dbtn.style.justifyContent = "end"
+
+    const message = document.createElement('p')
+    message.innerText = "message : here we are js"
+
+    const card = document.createElement('div')
+    card.style.width = "300px"
+    card.style.padding = "0 4% 3% 4%"
+    card.style.boxShadow = "0 2px 15px #ddd"
+
+    const area = document.createElement('div')
+    shadow.style.width = "100%"
+    shadow.style.height = "100vh"
+    shadow.style.dispaly = "flex"
+    shadow.style.justifyContent = "center"
+    shadow.style.alignItems = "center"
+    
+
+
+}
+
